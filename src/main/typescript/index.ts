43968e82dae4d23fb7eff5fc21fd06f81d2b7bc2
Xeno Snow Fox <xenosnowfox@gmail.com>
2023-01-01 22:00:03 +1100

    add check for local storage on application start up

--- src/main/typescript/index.ts
+++ src/main/typescript/index.ts
@@ -1,2 +1,15 @@
-let message: string = "Hello World";
-console.log(message);
+import localStorageAvailable from "./utils/local-storage-available";
+import FullPageError from "./components/full-page-error";
+
+// run main application
+(() => {
+	// The app requires the use of local storage in order to persist data.
+	if (!localStorageAvailable()) {
+		FullPageError.withMessage("Your browser is unable to run this application.").appendTo(document.body);
+		return;
+	}
+
+	// do any initial setup
+
+	// run the application
+})();
