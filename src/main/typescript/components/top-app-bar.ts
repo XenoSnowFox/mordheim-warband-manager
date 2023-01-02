9d17213e6b1bf43ed8a6eed2b19ba84c80b2dec1
Xeno Snow Fox <xenosnowfox@gmail.com>
2023-01-02 11:53:51 +1100

    Adds initial Top App Bar component

---
+++ src/main/typescript/components/top-app-bar.ts
@@ -0,0 +1,28 @@
+import View from "../sdk/view";
+
+export default class TopAppBar implements View {
+	private _title: string;
+
+	public constructor() {
+		this._title = "";
+	}
+
+	public set title(withTitle: string) {
+		this._title = withTitle;
+	}
+
+	public onDomLoad() {}
+
+	public onDomUnload() {}
+
+	public htmlElements(): Array<HTMLElement> {
+		var message = document.createElement("h1");
+		message.textContent = this._title;
+
+		var container = document.createElement("header");
+		container.appendChild(message);
+		container.classList.add("mwm-component-topAppBar");
+
+		return [container];
+	}
+}
