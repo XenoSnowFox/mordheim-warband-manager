a8541380994e1b6b4a13087b73d9b49ea959e4d6
Xeno Snow Fox <xenosnowfox@gmail.com>
2023-01-05 18:14:08 +1100

    update warband member summary card to include number of members and experience chips

--- src/main/typescript/components/warband-member-summary-card.ts
+++ src/main/typescript/components/warband-member-summary-card.ts
@@ -1,17 +1,24 @@
+import AssistChip from "../elements/assist-chip";
 import Card from "../elements/card";
 import { CardType } from "../enums/card-type";
 import { WarbandDesignation } from "../enums/warband-designation";
 import WarbandMemberModel from "../models/warband-member-model";
 import View from "../sdk/view";
+import dom from "../utils/dom";
+import svg from "../utils/svg";
 import UnitStatsGridView from "./unit-stats-grid";
 
 export default class WarbandMemberSummaryCard implements View {
 	private _container: Card = new Card();
+	private _chipsContainer: HTMLDivElement = document.createElement("div");
 	private _unitStatsGrid: UnitStatsGridView = new UnitStatsGridView();
 
 	public constructor() {
 		this._container.type = CardType.FILLED;
 		this._container.appendFooterView(this._unitStatsGrid);
+
+		this._chipsContainer.classList.add("mwm-component-warbandMemberSummaryCard-chips");
+		this._container.appendChild(this._chipsContainer);
 	}
 
 	public set warbandMember(withMember: WarbandMemberModel) {
@@ -25,6 +32,22 @@ export default class WarbandMemberSummaryCard implements View {
 				console.log("LEADER:", withMember);
 			},
 		});
+
+		while (this._chipsContainer.childNodes.length) {
+			this._chipsContainer.removeChild(this._chipsContainer.childNodes[0]);
+		}
+
+		if (withMember.designation === WarbandDesignation.HENCHMEN) {
+			const memberCountChip = new AssistChip();
+			memberCountChip.label = "" + withMember.memberCount;
+			memberCountChip.svg = svg.accountGroup();
+			dom.appendView(this._chipsContainer, memberCountChip);
+		}
+
+		const experienceChip = new AssistChip();
+		experienceChip.label = "" + withMember.experience;
+		experienceChip.svg = svg.school();
+		dom.appendView(this._chipsContainer, experienceChip);
 	}
 
 	public onDomLoad() {}
