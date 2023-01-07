0857bb7e66541923dd3d89e7fe94abe497a40082
Xeno Snow Fox <xenosnowfox@gmail.com>
2023-01-07 11:54:52 +1100

    Adding bottom sheets to the view manager

--- src/main/typescript/utils/view-manager.ts
+++ src/main/typescript/utils/view-manager.ts
@@ -1,6 +1,13 @@
+import BottomSheet from "../elements/bottom-sheet";
 import View from "../sdk/view";
+import dom from "./dom";
+
+interface VisibleNodes {
+	bottomSheet?: BottomSheet;
+}
 
 const viewStack: Array<View> = [];
+const visibleNodes: VisibleNodes = {};
 const domBody: HTMLElement = document.body;
 
 const renderDom = () => {
@@ -8,18 +15,18 @@ const renderDom = () => {
 		domBody.removeChild(domBody.lastChild);
 	}
 
-	if (viewStack.length == 0) {
-		return;
+	if (viewStack.length != 0) {
+		const currentView = viewStack.pop();
+		viewStack.push(currentView);
+		dom.appendView(domBody, currentView);
 	}
 
-	const currentView = viewStack.pop();
-	viewStack.push(currentView);
-	currentView.htmlElements().forEach((element) => domBody.appendChild(element));
-
-	setTimeout(() => window.scrollTo(0, 0), 1);
+	visibleNodes.bottomSheet && domBody.appendChild(visibleNodes.bottomSheet.rootElement);
 };
 
 const pushView = (nextView: View) => {
+	window.history.pushState(null, null, document.URL);
+
 	// call the unload on the previous
 	if (viewStack.length > 0) {
 		const currentView = viewStack.pop();
@@ -36,10 +43,15 @@ const pushView = (nextView: View) => {
 	// rerender the dom
 	renderDom();
 
-	window.history.pushState(null, null, document.URL);
+	setTimeout(() => window.scrollTo(0, 0), 1);
 };
 
 const popView = () => {
+	if (visibleNodes.bottomSheet) {
+		removeBottomSheet();
+		return;
+	}
+
 	// no items to remove
 	if (viewStack.length <= 1) {
 		return;
@@ -56,6 +68,29 @@ const popView = () => {
 
 	// rerender the dom
 	renderDom();
+
+	setTimeout(() => window.scrollTo(0, 0), 1);
+};
+
+const addBottomSheet = (withBottomSheet: BottomSheet) => {
+	window.history.pushState(null, null, document.URL);
+
+	removeBottomSheet();
+
+	visibleNodes.bottomSheet = withBottomSheet;
+
+	withBottomSheet.onShow();
+	domBody.appendChild(visibleNodes.bottomSheet.rootElement);
+};
+
+const removeBottomSheet = () => {
+	if (!visibleNodes.bottomSheet) {
+		return;
+	}
+
+	visibleNodes.bottomSheet.onDismiss();
+	visibleNodes.bottomSheet.rootElement.remove();
+	delete visibleNodes.bottomSheet;
 };
 
 window.addEventListener("popstate", (evt) => popView());
@@ -63,4 +98,6 @@ window.addEventListener("popstate", (evt) => popView());
 export default {
 	push: pushView,
 	pop: popView,
+
+	showBottomSheet: addBottomSheet,
 };
